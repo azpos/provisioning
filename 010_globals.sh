2defdd334ab4af46a5edb5e7bca9a3e548d07fbc
Panos Mavrogiorgos <pmav99@gmail.com>
2023-05-22 08:33:02 +0300

    Remove visualz

--- 010_globals.sh
+++ 010_globals.sh
@@ -12,7 +12,7 @@ export INFRA="infra"
 export COMPUTE="compute"
 export ARCHIVE="archive"
 export CONTROL="control"
-export VISUALZ="visualz"
+# export VISUALZ="visualz"
 
 # Prefixes
 export BASE_PREFIX="${PROJECT}"-"${ENVIRONMENT}"
@@ -20,7 +20,7 @@ export INFRA_PREFIX="${BASE_PREFIX}"-"${INFRA}"
 export ARCHIVE_PREFIX="${BASE_PREFIX}"-"${ARCHIVE}"
 export COMPUTE_PREFIX="${BASE_PREFIX}"-"${COMPUTE}"
 export CONTROL_PREFIX="${BASE_PREFIX}"-"${CONTROL}"
-export VISUALZ_PREFIX="${BASE_PREFIX}"-"${VISUALZ}"
+# export VISUALZ_PREFIX="${BASE_PREFIX}"-"${VISUALZ}"
 
 # Networking - Virtual Network
 export VNET="${BASE_PREFIX}"-vnet
@@ -29,12 +29,12 @@ export KEYVAULT="${BASE_PREFIX}"-kv
 # Networking - Subnets
 export COMPUTE_SNET="${COMPUTE_PREFIX}"-snet
 export CONTROL_SNET="${CONTROL_PREFIX}"-snet
-export VISUALZ_SNET="${VISUALZ_PREFIX}"-snet
+# export VISUALZ_SNET="${VISUALZ_PREFIX}"-snet
 
 # Networking - Network Security Groups
 export COMPUTE_NSG="${COMPUTE_PREFIX}"-nsg
 export CONTROL_NSG="${CONTROL_PREFIX}"-nsg
-export VISUALZ_NSG="${VISUALZ_PREFIX}"-nsg
+# export VISUALZ_NSG="${VISUALZ_PREFIX}"-nsg
 
 # SECRETS
 export COMPUTE_SSH_KEY="${COMPUTE}"-SSH-KEY
