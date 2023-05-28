93765d8467bc0a6bab3aa78953990a43a0df8168
Panos Mavrogiorgos <pmav99@gmail.com>
2023-05-28 16:29:10 +0300

    scripts: Implement JIT access

--- scripts/request_jit.sh
+++ scripts/request_jit.sh
@@ -2,25 +2,18 @@
 #
 
 set -euo pipefail
-# set -x
 
-access_token=$(az account get-access-token --subscription "${AZURE_SUBSCRIPTION_ID}" --query 'accessToken' --output tsv)
-url=https://management.azure.com/subscriptions/"${AZURE_SUBSCRIPTION_ID}"/resourceGroups/"${CONTROL_RG}"/providers/Microsoft.Security/locations/"${LOCATION}"/jitNetworkAccessPolicies/default?api-version=2020-01-01
+source ./azcli/010_globals.sh
+
 payload=$(jaz templates/jit_policy.json)
+az rest \
+  --method PUT \
+  --uri "/subscriptions/""${AZURE_SUBSCRIPTION_ID}""/resourceGroups/""${CONTROL_RG}""/providers/Microsoft.Security/locations/""${LOCATION}""/jitNetworkAccessPolicies/default?api-version=2020-01-01" \
+  --body "${payload}"
 
-http \
-  --verbose \
-  --check-status \
-  PUT \
-  "${url}" \
-  "authorization: Bearer ${access_token}" \
-  'Content-Type: application/json' <<< "${payload}"
 
-echo
-
-url=https://management.azure.com/subscriptions/"${AZURE_SUBSCRIPTION_ID}"/providers/Microsoft.Security/jitNetworkAccessPolicies?api-version=2020-01-01
-http \
-  --check-status \
-  "${url}" \
-  "authorization: Bearer ${access_token}" \
-  'Content-Type: application/json'
+payload=$(jaz templates/jit_access.json)
+az rest \
+  --method POST \
+  --uri "/subscriptions/""${AZURE_SUBSCRIPTION_ID}""/resourceGroups/""${CONTROL_RG}""/providers/Microsoft.Security/locations/""${LOCATION}""/jitNetworkAccessPolicies/default/initiate?api-version=2015-06-01-preview" \
+  --body "${payload}"
