d7714eada98effb1d6250820b638818ff7126df9
Panos Mavrogiorgos <pmav99@gmail.com>
2023-06-11 17:09:31 +0300

    Cloud-init: Use PYTHONUNBUFFERED=1

--- azcli/341_compute_cloud-init.sh
+++ azcli/341_compute_cloud-init.sh
@@ -3,4 +3,7 @@
 set -xeuo pipefail
 
 export ANSIBLE_CALLBACKS_ENABLED=ansible.posix.profile_tasks
-runuser --pty -u azuser -w ANSIBLE_CALLBACKS_ENABLED -- ansible-pull -U https://github.com/azpos/provisioning -i "$(hostname --short)," playbooks/common.yml playbooks/cluster_common.yml
+export PYTHONUNBUFFERED=1
+runuser --pty -u azuser -w PYTHONUNBUFFERED,ANSIBLE_CALLBACKS_ENABLED -- ansible-pull -U https://github.com/azpos/provisioning -i "$(hostname --short)," playbooks/common.yml playbooks/cluster_common.yml
+
+# sudo --preserve-env=ANSIBLE_CALLBACKS_ENABLED,PYTHONUNBUFFERED runuser --pty -u azuser -w PYTHONUNBUFFERED,ANSIBLE_CALLBACKS_ENABLED -- ansible-pull -U https://github.com/azpos/provisioning -i "$(hostname --short)," playbooks/common.yml playbooks/cluster_common.yml
