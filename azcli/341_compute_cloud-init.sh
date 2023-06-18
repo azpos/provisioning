8abcb777b7490037f7bf888eb8fd30d6790936d8
Panos Mavrogiorgos <pmav99@gmail.com>
2023-06-18 11:48:33 +0300

    azcli: Stop provisioning cluster.
    The provisioning will happen via seareport.

--- azcli/341_compute_cloud-init.sh
+++ azcli/341_compute_cloud-init.sh
@@ -2,8 +2,8 @@
 
 set -xeuo pipefail
 
-export ANSIBLE_CALLBACKS_ENABLED=ansible.posix.profile_tasks
-export PYTHONUNBUFFERED=1
-runuser --pty -u azuser -w PYTHONUNBUFFERED,ANSIBLE_CALLBACKS_ENABLED -- ansible-pull -U https://github.com/azpos/provisioning -i "$(hostname --short)," playbooks/common.yml playbooks/cluster_common.yml
+# export ANSIBLE_CALLBACKS_ENABLED=ansible.posix.profile_tasks
+# export PYTHONUNBUFFERED=1
+# runuser --pty -u azuser -w PYTHONUNBUFFERED,ANSIBLE_CALLBACKS_ENABLED -- ansible-pull -U https://github.com/azpos/provisioning -i "$(hostname --short)," playbooks/common.yml playbooks/cluster_common.yml
 
 # sudo --preserve-env=ANSIBLE_CALLBACKS_ENABLED,PYTHONUNBUFFERED runuser --pty -u azuser -w PYTHONUNBUFFERED,ANSIBLE_CALLBACKS_ENABLED -- ansible-pull -U https://github.com/azpos/provisioning -i "$(hostname --short)," playbooks/common.yml playbooks/cluster_common.yml
